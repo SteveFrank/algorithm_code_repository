3d716eaa0945c4b24a18d71d0e2743809a335f84
FrankQian <yangqian3@staff.weibo.com>
2021-01-25 10:06:07 +0800

    update -> LeetCode 1480

---
+++ src/main/java/com/leetcode/code/Solution1480.java
@@ -0,0 +1,21 @@
+package com.leetcode.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author yangqian
+ * @date 2021/1/23
+ */
+public class Solution1480 {
+
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return new int[]{};
+    }
+
+}
