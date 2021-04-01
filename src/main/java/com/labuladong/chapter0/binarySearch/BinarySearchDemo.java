0443e8f24e383f146d773f19446cc352f18d47a4
FrankQian <yangqian3@staff.weibo.com>
2021-04-01 14:37:59 +0800

    update : 二分查找问题

---
+++ src/main/java/com/labuladong/chapter0/binarySearch/BinarySearchDemo.java
@@ -0,0 +1,70 @@
+package com.labuladong.chapter0.binarySearch;
+
+/**
+ * @author yangqian
+ * @date 2021/4/1
+ */
+public class BinarySearchDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    int binarySearch(int[] nums, int target) {
+        int left = 0;
+        // 取值的真实下标长度
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int left_bound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    int right_bound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+
+}
