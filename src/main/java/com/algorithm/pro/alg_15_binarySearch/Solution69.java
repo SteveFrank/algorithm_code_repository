cc0e74c29b7808dcd66566b2acecb2bc12260541
FrankQian <yangqian3@staff.weibo.com>
2022-01-27 14:19:29 +0800

    update

--- src/main/java/com/algorithm/pro/alg_15_binarySearch/Solution69.java
+++ src/main/java/com/algorithm/pro/alg_15_binarySearch/Solution69.java
@@ -7,23 +7,29 @@ package com.algorithm.pro.alg_15_binarySearch;
 public class Solution69 {
 
     public int mySqrt(int x) {
-        // 特殊判断
-        if (x <= 1) {
-            return x;
+        // 特殊值判断
+        if (x == 0) {
+            return 0;
         }
-        // 找一个数k, k^2 <= x 找到一个最大的k就是我们需要的
-        long left = 1, right = x >> 1, mid = 0;
-        while (left <= right) {
-            mid = (left + right) >> 1;
-            if (mid * mid < x) {
-                left = mid + 1;
-            } else if (mid * mid > x) {
+        if (x == 1) {
+            return 1;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        // 在区间 [left..right] 查找目标元素
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            // 注意：这里为了避免乘法溢出，改用除法
+            if (mid > x / mid) {
+                // 下一轮搜索区间是 [left..mid - 1]
                 right = mid - 1;
             } else {
-                return (int) mid;
+                // 下一轮搜索区间是 [mid..right]
+                left = mid;
             }
         }
-        return (int) left - 1;
+        return left;
     }
 
     public static void main(String[] args) {
