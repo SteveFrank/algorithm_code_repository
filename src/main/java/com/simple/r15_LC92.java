619ba456b28ab2fb48bca4859757850009b853af
FrankQian <yangqian3@staff.weibo.com>
2022-02-14 17:05:42 +0800

    update : 链表反转 N序

---
+++ src/main/java/com/simple/r15_LC92.java
@@ -0,0 +1,34 @@
+package com.simple;
+
+/**
+ * @author frankq
+ * @date 2022/2/14
+ */
+public class r15_LC92 {
+
+    private ListNode successor = null;
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+}
