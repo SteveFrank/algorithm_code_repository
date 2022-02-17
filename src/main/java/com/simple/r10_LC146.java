afc82b1397991f8b73c19f66cee3d0c55e635f96
FrankQian <yangqian3@staff.weibo.com>
2022-02-17 15:29:41 +0800

    update : lru

--- src/main/java/com/simple/r10_LC146.java
+++ src/main/java/com/simple/r10_LC146.java
@@ -1,5 +1,8 @@
 package com.simple;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @author frankq
  * @date 2022/2/12
@@ -12,8 +15,33 @@ public class r10_LC146 {
 
 }
 
-class LRUCache {
-    class Node {
+/**
+ * 基于LinkedList的实现方法
+ */
+class LRUCacheWithLinkedList extends LinkedHashMap<Integer, Integer> {
 
+    private int capacity;
+
+    // 构造方法
+    public LRUCacheWithLinkedList(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    // 获取数据
+    public int get(int key) {
+        // 需要表示出当前数据已经使用
+        return super.get(key);
+    }
+
+    // 将数据放入缓存
+    public void put(int key, int value) {
+        // 缓存容量如果达到上限需要进行呢删除操作
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
---
+++ src/main/java/com/simple/r10_LC146_custom.java
@@ -0,0 +1,120 @@
+package com.simple;
+
+import java.util.HashMap;
+
+/**
+ * @author frankq
+ * @date 2022/2/17
+ */
+public class r10_LC146_custom {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/**
+ * 自定义实现HashMap 双向链表的缓存机制
+ */
+class LRUCache {
+    /**
+     * 定义双向链表的节点类
+     */
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        public Node() {}
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    // 定义哈希表
+    private HashMap<Integer, Node> hashMap = new HashMap<>();
+    // 定义属性
+    private int capacity;
+    private int size;
+    // 定义头尾指针
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+
+        // 用节点定义哨兵，方便统一处理
+        head = new Node();
+        tail = new Node();
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // get方法
+    public int get(int key) {
+        // 从hash表中查找key，如果不存在的话就返回-1
+        Node node = hashMap.get(key);
+        if (node == null) {
+            return -1;
+        }
+        // 如果存在则移动到链表末尾
+        moveToTail(node);
+        return node.value;
+    }
+
+    // put 操作
+    public void put(int key, int value) {
+        // 同样现在哈希表中查找key
+        Node node = hashMap.get(key);
+        // 如果key存在，修改value 并且移动到末尾
+        if (node != null) {
+            node.value = value;
+            moveToTail(node);
+        } else {
+            // 如果不存在 需要创建新的节点，插入到末尾
+            Node newNode = new Node(key, value);
+            hashMap.put(key, newNode);
+            addToTail(newNode);
+            size ++;
+            // 如果超出了容量，需要删除链表的头节点
+            if (size > capacity) {
+                Node head = removeHead();
+                hashMap.remove(head.key);
+                size--;
+            }
+        }
+    }
+
+    // 移动节点到末尾
+    private void moveToTail(Node node) {
+        // 移除节点
+        removeNode(node);
+        // 添加节点到末尾
+        addToTail(node);
+    }
+
+    // 删除节点中的某一个节点
+    private void removeNode(Node node) {
+        // 跳过当前node
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // 在链表末尾增加一个节点
+    private void addToTail(Node node) {
+        node.next = tail;
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+
+    // 删除头节点
+    private Node removeHead() {
+        Node realHead = head.next;
+        removeNode(realHead);
+        return realHead;
+    }
+
+}
