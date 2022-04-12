5e96941d4f56446af21f05d10084dd383865c546
FrankQian <yangqian3@staff.weibo.com>
2022-04-12 15:46:56 +0800

    update

--- src/main/java/com/simple/r04_LC20.java
+++ src/main/java/com/simple/r04_LC20.java
@@ -29,7 +29,7 @@ public class r04_LC20 {
                 stack.push(')');
             } else if (c == '{') {
                 stack.push('}');
-            } else if (c == '}') {
+            } else if (c == '[') {
                 stack.push(']');
             } else if (stack.isEmpty() || c != stack.pop()) {
                 return false;
