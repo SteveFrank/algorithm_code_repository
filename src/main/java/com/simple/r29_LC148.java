c240e9621648694f7df0882601f4525cb685afe4
FrankQian <yangqian3@staff.weibo.com>
2022-04-14 21:00:56 +0800

    add : 题解

--- src/main/java/com/simple/r29_LC148.java
+++ src/main/java/com/simple/r29_LC148.java
@@ -11,34 +11,51 @@ public class r29_LC148 {
     }
 
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
             return head;
         }
-        ListNode fast = head.next, slow = head;
-        // 找中点
-        while (fast != null && fast.next != null) {
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
             slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode temp = slow.next;
-        slow.next = null;
-        // 分开递归排序再合并
-        ListNode left = sortList(head);
-        ListNode right = sortList(temp);
-        ListNode h = new ListNode(0);
-        ListNode res = h;
-        while (left != null && right != null) {
-            if (left.data < res.data) {
-                h.next = left;
-                left = left.next;
-            } else {
-                h.next = right;
-                right = right.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
             }
-            h = h.next;
         }
-        h.next = left != null ? left : right;
-        return res.next;
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.data <= temp2.data) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
     }
 
 }
